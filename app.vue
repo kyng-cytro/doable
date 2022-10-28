3f7df0c094c96e8576995d48ac6c0e5a69f9b476
Kyng Cytro <johndibashi2001@gamil.com>
2022-10-28 02:42:41 +0100

    :tada: first commit & navbar

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div :class="{ dark: darkMode }">
+    <NavBar />
+    <div class="text-red-500 dark:text-yellow-300">Test Text</div>
+  </div>
+</template>
+
+<script setup>
+const darkMode = ref(false);
+</script>
---
+++ components/NavBar/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="flex items-center border-2 justify-between border-red-500 py-6 px-6"
+  >
+    <div class="flex items-center gap-10">
+      <NuxtLink to="/">
+        <h3 class="text-xl hover:text-blue-400 focus:text-blue-400">doAble</h3>
+      </NuxtLink>
+      <div class="hidden items-center gap-6 sm:flex">
+        <NuxtLink to="/">
+          <span class="hover:text-blue-400 focus:text-blue-400">Features</span>
+        </NuxtLink>
+        <NuxtLink to="/">
+          <span class="hover:text-blue-400 focus:text-blue-400">Templates</span>
+        </NuxtLink>
+        <NuxtLink to="/">
+          <span class="hover:text-blue-400 focus:text-blue-400">Pricing</span>
+        </NuxtLink>
+      </div>
+    </div>
+    <div class="hidden items-center gap-6 sm:flex">
+      <NuxtLink to="/">
+        <span class="hover:text-blue-400 focus:text-blue-400">Log in</span>
+      </NuxtLink>
+      <NuxtLink to="/">
+        <span
+          class="bg-blue-400 rounded-full px-6 py-3 text-white hover:bg-blue-600 focus:bg-blue-600"
+          >Get Started</span
+        >
+      </NuxtLink>
+    </div>
+    <div></div>
+  </div>
+</template>
